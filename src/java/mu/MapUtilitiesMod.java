adf7eb6f4a0d09760fb75bb14aa876e800da7cbe
ApsZoldat <apszoldat@gmail.com>
2024-03-22 12:35:03 +0300

    Something

--- src/java/mu/MapUtilitiesMod.java
+++ src/java/mu/MapUtilitiesMod.java
@@ -2,10 +2,13 @@ package mu;
 
 import arc.Events;
 import arc.util.Reflect;
+import arc.util.Log;
 import mindustry.core.Version;
 import mindustry.editor.MapInfoDialog;
 import mindustry.game.EventType;
 import mindustry.mod.Mod;
+import mindustry.ui.dialogs.CustomGameDialog;
+import mindustry.ui.dialogs.CustomRulesDialog;
 import mindustry.ui.dialogs.MapPlayDialog;
 import mu.legacy.modifying.ui.LegacyRulesDialog;
 import mu.legacy.modifying.ui.LegacySettingsDialog;
@@ -17,15 +20,14 @@ import static mindustry.Vars.ui;
 public class MapUtilitiesMod extends Mod{
     public MapUtilitiesMod() {
         Events.on(EventType.ClientLoadEvent.class, e -> {
-            MapInfoDialog infoDialog = Reflect.get(ui.editor, "infoDialog");
-            MapPlayDialog playDialog = Reflect.get(ui.custom, "dialog");
-            MapPlayDialog playtestDialog = Reflect.get(ui.editor, "playtestDialog");
-
+            CustomRulesDialog infoRules = Reflect.get(MapInfoDialog.class, Reflect.get(ui.editor, "infoDialog"), "ruleInfo");
+            CustomRulesDialog playRules = Reflect.get(MapPlayDialog.class, Reflect.get(ui.custom, "dialog"), "dialog");
+            CustomRulesDialog playtestRules = Reflect.get(MapPlayDialog.class, Reflect.get(ui.editor, "playtestDialog"), "dialog");
 
             if(0 <= Version.build && Version.build <= 146){
-                LegacyRulesDialog.modify(Reflect.get(infoDialog, "ruleInfo"));
-                LegacyRulesDialog.modify(Reflect.get(playDialog, "dialog"));
-                LegacyRulesDialog.modify(Reflect.get(playtestDialog, "dialog"));
+                LegacyRulesDialog.modify(infoRules);
+                LegacyRulesDialog.modify(playRules);
+                LegacyRulesDialog.modify(playtestRules);
                 LegacySettingsDialog.modify();
             }else{
 
